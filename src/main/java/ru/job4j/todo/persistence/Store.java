488f620de2e1442027e26e1172ff56d07f90e3c5
Roman Pogorelov <rmk140987@gmail.com>
2021-09-15 17:33:27 +0300

    3. Лямбды и шаблон wrapper. [#49295]

--- src/main/java/ru/job4j/todo/persistence/Store.java
+++ src/main/java/ru/job4j/todo/persistence/Store.java
@@ -2,13 +2,14 @@ package ru.job4j.todo.persistence;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.job4j.todo.model.Item;
 
-import java.sql.Timestamp;
 import java.util.List;
+import java.util.function.Function;
 
 public class Store {
     private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
@@ -16,39 +17,29 @@ public class Store {
     private final SessionFactory sf = new MetadataSources(registry)
             .buildMetadata().buildSessionFactory();
 
-    public static void main(String[] args) {
-        Store store = new Store();
-        store.saveOrUpdate(new Item("descr1", new Timestamp(System.currentTimeMillis()), false));
-        store.saveOrUpdate(new Item("descr2", new Timestamp(System.currentTimeMillis()), true));
-        store.saveOrUpdate(new Item("descr3", new Timestamp(System.currentTimeMillis()), false));
-        store.saveOrUpdate(new Item("descr4", new Timestamp(System.currentTimeMillis()), false));
-    }
-
     public List<Item> findAll() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List items = session.createQuery("from Item").list();
-        session.getTransaction().commit();
-        session.close();
-        return items;
+        return tx(session -> session.createQuery("from Item").list());
     }
 
     public Item saveOrUpdate(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.saveOrUpdate(item);
-        session.getTransaction().commit();
-        session.close();
-        return item;
+        return tx(session -> {
+            session.saveOrUpdate(item);
+            return item;
+        });
     }
 
-    public boolean replace(int id, Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        item.setId(id);
-        session.update(item);
-        session.getTransaction().commit();
-        session.close();
-        return true;
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 }
